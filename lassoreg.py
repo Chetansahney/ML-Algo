06b1013500a41191b11023d9c639a40fa15a9e37
Chetan Sahney <84283700+Chetansahney@users.noreply.github.com>
2025-10-04 16:04:00 +0530

    Update lassoreg.py

--- lassoreg.py
+++ lassoreg.py
@@ -25,7 +25,7 @@ y=pd.DataFrame(data.target,columns=["target"])
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2, random_state=0)
 
-#Step 3: implementing Linear regression
+#Step 3: implementing Lasso regression
 
 regressor=Lasso(alpha=1.0)
 regressor.fit(x_train,y_train)
